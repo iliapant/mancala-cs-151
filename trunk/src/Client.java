6e00e45510a8c415f12858604d8ead02a03e98eb
vladut.mail@gmail.com <vladut.mail@gmail.com@8c2f3a09-81bc-e39f-bf86-1ad91abe2936>
2011-04-26 00:56:31 +0000

    First commit.

---
+++ trunk/src/Client.java
@@ -0,0 +1,16 @@
+/**
+ * Client class.
+ */
+public class Client {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		// Start the mancala from here.
+
+	}
+
+}
