6bf3361aa309ca0090e533acd1ac4e529680cf88
daniel.demicco@gmail.com <daniel.demicco@gmail.com@8c2f3a09-81bc-e39f-bf86-1ad91abe2936>
2011-04-27 03:36:22 +0000

    Set up the board/pits

---
+++ trunk/src/view/Pit.java
@@ -0,0 +1,13 @@
+package view;
+
+import java.awt.geom.Ellipse2D;
+
+public class Pit extends MancalaShape
+{
+    public Pit(int x, int y, int width, int height)
+    {
+        super(x, y, width, height);
+        Ellipse2D.Double pit = new Ellipse2D.Double(x, y, width, height);
+        super.add(pit);
+    }
+}
