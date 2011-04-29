e8bbe86747771b7936053f0e60777f9543d80ddb
vladut.mail@gmail.com <vladut.mail@gmail.com@8c2f3a09-81bc-e39f-bf86-1ad91abe2936>
2011-04-29 00:22:50 +0000

    Started on the GameModel. canMakeMove is done.

--- src/model/GameModel.java
+++ src/model/GameModel.java
@@ -10,22 +10,76 @@ import javax.swing.event.ChangeListener;
  */
 public class GameModel implements ChangeListener
 {
+	public static final int PITS_NUM = 14;
+	public static final int PLAYER_A_MANCALA = 6;
+	public static final int PLAYER_B_MANCALA = 13;
+	public static enum GameState {PLACING, ONGOING, ENDED};
+	public static enum Player {A, B};
 	private int[] pits;
-	private int player;
+	private Player currentPlayer;
+	private GameState currentState;
+	
+	/**
+	 * Basic Constructor of a GameModel.
+	 */
+	public GameModel()
+	{
+		pits = new int[PITS_NUM];
+		currentPlayer = Player.A;
+		currentState = GameState.PLACING;
+	}
+	
+	/**
+	 * Given a valid pitIndex, determine who owns it.
+	 * 
+	 * @param pitIndex
+	 * @return the player that owns the pit
+	 */
+	
+	private Player getPitOwner(int pitIndex)
+	{
+		if (pitIndex >= 0 && pitIndex < PLAYER_A_MANCALA)
+			return Player.A;
+		// If not assume it's player B's
+		return Player.B;
+	}
+	
+	/**
+	 * Checks if moving from pitIndex is possible.
+	 * 
+	 * @param pitIndex the index of the pit
+	 * @return true if move can be made, false otherwise
+	 */
 	public boolean canMakeMove(int pitIndex)
 	{
-		return false;
+		// Moves can be made only from valid pits
+		if (pitIndex < 0 || pitIndex > PITS_NUM
+				|| pitIndex == PLAYER_A_MANCALA
+				|| pitIndex == PLAYER_B_MANCALA)
+			return false;
+		
+		// Moves can be made only if the state of the game is ONGOING
+		if (currentState != GameState.ONGOING)
+			return false;
+		
+		// Moves can be made only from non-empty pits
+		if (pits[pitIndex] == 0)
+			return false;
+		
+		// Moves can be made only if the player owns the pit
+		if (getPitOwner(pitIndex) != currentPlayer)
+			return false;
+		
+		return true;
 	}
 	public void makeMove(int pitIndex)
 	{
 		
 	}
-	// TODO: everything
 	@Override
 	public void stateChanged(ChangeEvent e)
 	{
 		// TODO Auto-generated method stub
 
 	}
-
 }
